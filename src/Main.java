7ecc46e34674af170ea726256e544edae1aa52d3
mukesh007kls <mallikanti2000@gmail.com>
2023-02-23 15:06:50 +0530

    ADDED:Code till the player reachs the final position

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
 public class Main {
-    public static final int LADDER=1;
-    public static final int SNAKE=2;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+    public static int WIN_POSITION = 100;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game Program");
-        int startPos=0;
-        int rollingDice=(int) (Math.random()*10)%6 + 1;
-        //Snake or ladder option--sol
-        int sol=(int) (Math.random()*10)%3;
-        switch (sol){
-            case LADDER:
-                startPos+=rollingDice;
-                break;
-            case SNAKE:
-                startPos-=rollingDice;
-                break;
+        int startPos = 0;
+
+        while (startPos <= 100) {
+            int rollingDice = (int) (Math.random() * 10) % 6 + 1;
+            //Snake or ladder option--sol
+            int sol = (int) (Math.random() * 10) % 3;
+
+            switch (sol) {
+                case LADDER:
+                    startPos += rollingDice;
+                    break;
+                case SNAKE:
+                    startPos -= rollingDice;
+                    break;
+            }
+
+            if(startPos<0){
+                startPos=0;
+            }
+
+            System.out.println("Position of player:-"+startPos);
         }
-        System.out.println(startPos);
     }
 }
