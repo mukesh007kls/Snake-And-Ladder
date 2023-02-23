e5a22addc48fe92da88972c9014473e06492a83a
mukesh007kls <mallikanti2000@gmail.com>
2023-02-23 15:47:38 +0530

    ADDED:Code to print number of times dice rolled and positon

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game Program");
         int startPos = 0;
+        //number of time dice rolled -- notdr
+        int notdr = 0;
 
         while (startPos <= WIN_POSITION) {
             int rollingDice = (int) (Math.random() * 10) % 6 + 1;
+            notdr++;
             //Snake or ladder option--sol
             int sol = (int) (Math.random() * 10) % 3;
 
             switch (sol) {
                 case LADDER:
                     startPos += rollingDice;
-                    if(startPos>WIN_POSITION){
-                        startPos-=rollingDice;
+                    if (startPos > WIN_POSITION) {
+                        startPos -= rollingDice;
                     }
                     break;
                 case SNAKE:
@@ -26,16 +29,16 @@ public class Main {
                     break;
             }
 
-            if(startPos<0){
-                startPos=0;
+            if (startPos < 0) {
+                startPos = 0;
             }
 
-            if(startPos==WIN_POSITION){
-                System.out.println("Player Reached Win Position");
+            if (startPos == WIN_POSITION) {
+                System.out.println("Player Reached Win Position:- " + startPos + " Number of time dice rolled:- " + notdr);
                 break;
             }
 
-            System.out.println("Position of player:-"+startPos);
+            System.out.println("Position of player:- " + startPos + " Number of time dice rolled:- " + notdr);
         }
 
     }
