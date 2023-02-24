222aaaff0df7075d823d4ed6b142adde8295c4b2
mukesh007kls <123714212+mukesh007kls@users.noreply.github.com>
2023-02-24 08:32:07 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ public class Main {
     public static final int SNAKE=2;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game Program");
-        int startPos=0;
+        int startPosition=0;
         int rollingDice=(int) (Math.random()*10)%6 + 1;
         //Snake or ladder option--sol
         int sol=(int) (Math.random()*10)%3;
         switch (sol){
             case LADDER:
-                startPos+=rollingDice;
+                startPosition+=rollingDice;
                 break;
             case SNAKE:
-                startPos-=rollingDice;
+                startPosition-=rollingDice;
                 break;
         }
-        System.out.println(startPos);
+        System.out.println(startPosition);
     }
 }
