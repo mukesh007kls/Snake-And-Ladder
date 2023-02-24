3888299c995580e80fd78f232a944f1df65ba556
mukesh007kls <123714212+mukesh007kls@users.noreply.github.com>
2023-02-24 08:30:56 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game Program");
-        int startPos=0;
+        int startPosition=0;
     }
 }
